554a02884a06f5b6aab33595e24b49f5423e5128
bradleymeck <bradley.meck@gmail.com>
2015-12-01 18:58:31 -0600

    slightly flesh out example

--- example.js
+++ example.js
@@ -1,4 +1,21 @@
+const hd = require("heapdump");
+const hds = require("heapdump-sample");
+
+// turn on id tracking so sampling works
+hds.startTrackingHeapObjects();
+
+// use an FD here to get it to print data out in real time (not just in snapshots)
+// note: if you see the same sample index with different values, something got GC'd
+setInterval(_ => hds.sample(process.stdout.fd), 100);
+
+// make objects as quick as we can, for visible sample sizes
 const a = [];
-setTimeout(process.exit, 400);
-setInterval(_ => require("./").sample(2), 100);
 setInterval(_ => a.push({}))
+
+setTimeout(
+    _ => {
+      // this will produce a snapshot with samples array filled out
+      hd.writeSnapshot("my.heapsnapshot");
+      process.exit();
+    }, 400);
+
